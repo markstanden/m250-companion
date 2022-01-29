c379f5698c7faf098f9f0c36012b4ef02e948084
Mark Standen <mark@markstanden.dev>
2022-01-29 09:53:41 +0000

    adding seed functionality

--- src/main/java/C6/Activities/Act15/L33tNameGenerator.java
+++ src/main/java/C6/Activities/Act15/L33tNameGenerator.java
@@ -1,5 +1,7 @@
 package C6.Activities.Act15;
 
+import java.util.Random;
+
 /**
  * Activity 15
  * Went a little off piste with this one
@@ -10,13 +12,24 @@ public class L33tNameGenerator {
     private final String PLACE;
     private final String LANG;
 
+    private Random rand;
+
     /**
      * Want to play with a private constructor and a static builder method.
      */
-    private L33tNameGenerator(String name, String place, String lang){
+    private L33tNameGenerator(int seed, String name, String place, String lang){
         NAME = standardise(name);
         PLACE = standardise(place);
         LANG = standardise(lang);
+        rand = new Random(seed);
+    }
+
+    /**
+     * Static method returning a new builder
+     * @return a new builder for the L33tNameGenerator
+     */
+    public static Builder create(){
+        return new Builder();
     }
 
     /**
@@ -28,14 +41,47 @@ public class L33tNameGenerator {
         return word.trim().toLowerCase();
     }
 
-    /**
-     * Static method returning a new builder
-     * @return a new builder for the L33tNameGenerator
-     */
-    public static Builder create(){
-        return new Builder();
+
+    private int rand(int number) {
+        return rand.nextInt(1,4);
     }
 
+
+
+
+
+    public static class Builder {
+
+        private String name = "";
+        private String place = "";
+        private String lang = "";
+        private int seed;
+
+        public L33tNameGenerator build(){
+            return new L33tNameGenerator(seed, name, place, lang);
+        }
+
+        public Builder withName(String name){
+            this.name = name;
+            return this;
+        }
+
+        public Builder withPlace(String place){
+            this.place = place;
+            return this;
+        }
+
+        public Builder withLang(String lang){
+            this.lang = lang;
+            return this;
+        }
+
+        public Builder withSeed(int seed){
+            this.seed = seed;
+            return this;
+        }
+
+    }
     /**
      * Courtesy method to test the builder only.
      * Would not form part of the public API
@@ -64,31 +110,5 @@ public class L33tNameGenerator {
     @Deprecated
     public String getLANG() {
         return LANG;
-    }
-
-
-    public static class Builder {
-        private String name = "";
-        private String place = "";
-        private String lang = "";
-
-        public L33tNameGenerator build(){
-            return new L33tNameGenerator(name, place, lang);
-        }
-
-        public Builder withName(String name){
-            this.name = name;
-            return this;
-        }
-
-        public Builder withPlace(String place){
-            this.place = place;
-            return this;
-        }
-
-        public Builder withLang(String lang){
-            this.lang = lang;
-            return this;
-        }
     }
 }
