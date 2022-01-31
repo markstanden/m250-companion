e7bd84dfff09143011be4f1ab81d837309cbdc3f
Mark Standen <mark@markstanden.dev>
2022-01-31 06:12:34 +0000

    prepare act 26

---
+++ src/main/java/C6/Activities/Act26/Test.java
@@ -0,0 +1,24 @@
+package C6.Activities.Act26;
+
+import java.util.HashMap;
+
+public class Test {
+    public HashMap<Integer, String> myMap;
+
+    public Test() {
+        myMap = new HashMap<>();
+    }
+
+    /**
+     * The method should first check if the key given
+     * by the first parameter already exists in myMap .
+     * If the key does not already exist in myMap,
+     * a new entry should be created with a key given by
+     * the first parameter and a value given by the second parameter.
+     * If the key does already exist in myMap
+     * the associated value should remain unchanged.
+     */
+    public void populateMap(Integer key, String value) {
+
+    }
+}
---
+++ src/test/java/C6/Activities/Act26/TestTest.java
@@ -0,0 +1,17 @@
+package C6.Activities.Act26;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void populateMap() {
+    }
+}
