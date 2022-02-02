c044925c9dd17fb85fddf22e3e32ca24327d3078
Mark Standen <mark@markstanden.dev>
2022-02-02 16:15:35 +0000

    regex a bit neater

--- src/main/java/C7/Activities/Act20/Kabaddi/Kabaddi.java
+++ src/main/java/C7/Activities/Act20/Kabaddi/Kabaddi.java
@@ -3,6 +3,7 @@ package C7.Activities.Act20.Kabaddi;
 import C7.Activities.Act23.Patient;
 
 import java.util.Arrays;
+import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,12 +18,21 @@ public class Kabaddi
     }
 
     public void printTeamSurnamesFun(String allNames) {
-        //"Name1 Surname1, Name2 Surname2, Name3 Surname3"
+        //"Name1 Surname1,Name2 Surname2,Name3 Surname3"
+        /*
+            \b      surname starts at the start of a word block
+            ()      groups the item we want to extract
+            \w      alphanumeric characters (and underscore)
+            +       matches 1 or more
+            (?= )   matches ahead of the group, but doesn't include it,
+                    our surnames will either end with a comma or the end of string
+            ,|$     comma or end of string, this will follow our group
+         */
         Pattern p = Pattern.compile("\\b(\\w+)(?=,|$)");
         Matcher m = p.matcher(allNames);
         m.results()
-                .map(result -> result.group())
-                .forEach(surname -> System.out.println(surname));
+                .map(MatchResult::group)
+                .forEach(System.out::println);
     }
 
     public void printTeamSurnamesFunSimple(String allNames) {
